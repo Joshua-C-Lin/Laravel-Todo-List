2e08ade7567f4061d6d69080df395aeb273c77c7
Joshua.C.Lin <joshuaclin77@gmail.com>
2024-02-26 11:07:16 +0800

    First Commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+    public function index() {
+        // Retrieve all of the tasks when we visit the homepage
+        $tasks = Task::orderBy('completed_at')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function create() {
+        return view('tasks.create');
+    }
+
+    public function store() {
+        request()->validate([
+            'description' => 'required|max:255',
+        ]);
+
+        $task = Task::create([
+            'description' => request('description'),
+        ]);
+
+        return redirect('/');
+    }
+
+    public function update($id) {
+        $task = Task::where('id', $id)->first();
+
+        $task->completed_at = now();
+        $task->save();
+
+        return redirect('/');
+    }
+
+    public function delete($id) {
+        $task = Task::where('id', $id)->first();
+
+        $task->delete();
+
+        return redirect('/');
+    }
+
+    // Todo add EDIT function
+}
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Joshua Task List</h1>
+
+    @foreach($tasks as $task)
+    <div class="card @if($task->isCompleted()) border-success @endif" style="margin-bottom: 20px;">
+        <div class="card-body">
+            <p>
+                @if($task->isCompleted())
+                    <span class="badge bg-success">Completed</span>
+                @endif
+                {{ $task->description }}
+            </p>
+
+            @if(!$task->isCompleted())
+                <form action="/tasks/{{ $task->id }}" method="POST">
+                    @method("PATCH")
+                    @csrf
+                    <button class="btn btn-secondary" input="submit">Complete</button>
+                </form>
+            @else
+                <form action="/tasks/{{ $task->id }}" method="POST">
+                    @method("DELETE")
+                    @csrf
+                    <button class="btn btn-danger" input="submit">Delete</button>
+                </form>
+            @endif
+
+        </div>
+    </div>
+    @endforeach
+
+    <a href="/tasks/create" class="btn btn-primary btn-block">New Task</a>
+@endsection
